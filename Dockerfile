d4234eba5dd6220ffb63b98596a13b7657f779f5
tobiashi26 <tobiashoughton2@gmail.com>
2024-10-19 15:28:53 -0500

    changing pygeoflood-container to better match template

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,23 @@
 # Use a base image with Conda installed
 FROM continuumio/miniconda3
 
-# Set the working directory
-WORKDIR /usr/src/app
-
 # Update the package list and install build-essential
 RUN apt-get update && \
     apt-get install -y build-essential && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
-# Optionally, copy your other application files
-COPY . .
+# Copy your application files
+COPY --chmod=755 run.sh /tapis/run.sh
+COPY main.py /code/main.py
 
-# Create the conda environment with the necessary dependencies
-RUN conda create --name pygeoflood-env python=3.11
+# Create the conda environment with the necessary dependencies and install pygeoflood
+RUN conda create --name pygeoflood-env python=3.11 --yes && \
+    conda run -n pygeoflood-env pip install git+https://github.com/tobiashi26/pygeoflood.git && \
+    conda clean --all --yes  # Clean up unnecessary files to reduce image size
 
-# Activate the environment and install pygeoflood
-RUN /bin/bash -c "source activate pygeoflood-env && pip install git+https://github.com/tobiashi26/pygeoflood.git && pip list"
-
-# Ensure the `run.sh` script is executable (if it exists)
-RUN chmod +x ./run.sh
+# Ensure the `run.sh` script is executable
+RUN chmod +x /tapis/run.sh
 
 # Set the entry point for the container with Conda environment activation
-ENTRYPOINT ["/bin/bash", "-c", "source activate pygeoflood-env && ./run.sh"]
+ENTRYPOINT ["/bin/bash", "-c", "source activate pygeoflood-env && /tapis/run.sh"]
--- main.py
+++ main.py
@@ -1,4 +1,51 @@
-from app import pygeoflood_full_workflow
+from pathlib import Path
+from pygeoflood import PyGeoFlood
+
+def pygeoflood_full_workflow(dem, flowlines, catchments, streamflow):
+
+    pgf = PyGeoFlood(dem_path = dem)
+
+    pgf.flowline_path = flowlines
+
+    pgf.catchment_path = catchments
+
+    pgf.streamflow_forecast_path = streamflow
+
+    pgf.apply_nonlinear_filter()
+
+    pgf.calculate_slope()
+
+    pgf.calculate_curvature()
+
+    pgf.fill_depressions()
+
+    pgf.calculate_mfd_flow_accumulation()
+
+    pgf.calculate_d8_flow_direction()
+
+    pgf.find_outlets()
+
+    pgf.delineate_basins()
+
+    pgf.define_skeleton()
+
+    pgf.calculate_binary_hand()
+
+    pgf.find_endpoints()
+
+    pgf.extract_channel_network()
+
+    pgf.calculate_hand()
+
+    pgf.segment_channel_network()
+
+    pgf.delineate_segment_catchments()
+
+    pgf.calculate_src()
+
+    pgf.calculate_flood_stage()
+
+    pgf.inundate()
 
 def parse_args():
     import argparse
@@ -12,12 +59,10 @@ def parse_args():
     parser.add_argument("streamflow", help="")
     return parser.parse_args()
 
-if __name__ == "__main__":
+args = parse_args()
+dem = args.DEM
+flowlines = args.flowlines
+catchments = args.catchments
+streamflow = args.streamflow
 
-    args = parse_args()
-    dem = args.DEM
-    flowlines = args.flowlines
-    catchments = args.catchments
-    streamflow = args.streamflow
-
-    pygeoflood_full_workflow.pygeoflood_woodville(dem, flowlines, catchments, streamflow)
+pygeoflood_full_workflow.pygeoflood_woodville(dem, flowlines, catchments, streamflow)

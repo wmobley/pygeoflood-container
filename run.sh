0e429361ce3fd66c46a55741baa551eccf5b6ace
tobiashi26 <tobiashoughton2@gmail.com>
2024-10-19 13:56:38 -0500

    Updating fileInputs in .json file
    Updating fileInputs in .json file to "Catchment". This will be the directory that contains the input files for a given catchment

--- run.sh
+++ run.sh
@@ -4,9 +4,8 @@ set -xe
 DEM = $1
 Streamflow = $2
 
-python main.py ${_tapisExecSystemInputDir}/{DEM}.tif ${_tapisExecSystemInputDir}/Catchment.shp ${_tapisExecSystemInputDir}/Flowlines.shp ${_tapisExecSystemInputDir}/{Streamflow}_Streamflow.csv
+python main.py Catchment/{DEM}.tif Catchment/Catchment.shp Catchment/Flowlines.shp Catchment/{Streamflow}_Streamflow.csv
 
-
-cp /usr/src/app/{Catchment}/{DEM}_src.csv $_tapisExecSystemOutputDir/{DEM}_src.csv
-cp /usr/src/app/{Catchment}/{DEM}_HAND.tif $_tapisExecSystemOutputDir/{DEM}_HAND.tif
-cp /usr/src/app/{Catchment}/{DEM}_segment_catchments.tif $_tapisExecSystemOutputDir/{DEM}_segment_catchments.tif
+cp /usr/src/app/Catchment/{DEM}_src.csv $_tapisExecSystemOutputDir/{DEM}_src.csv
+cp /usr/src/app/Catchment/{DEM}_HAND.tif $_tapisExecSystemOutputDir/{DEM}_HAND.tif
+cp /usr/src/app/Catchment/{DEM}_segment_catchments.tif $_tapisExecSystemOutputDir/{DEM}_segment_catchments.tif

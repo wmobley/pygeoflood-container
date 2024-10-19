111855507fb63aba87c7f036c29ac039a91d12f0
tobiashi26 <tobiashoughton2@gmail.com>
2024-10-19 15:49:37 -0500

    correcting run.sh
    correcting assignment of input variables in run.sh and cp paths

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -xe
 
-DEM = $1
-Streamflow = $2
+DEM=$1
+Streamflow=$2
 
 python main.py Catchment/{DEM}.tif Catchment/Catchment.shp Catchment/Flowlines.shp Catchment/{Streamflow}_Streamflow.csv
 
-cp /usr/src/app/Catchment/{DEM}_src.csv $_tapisExecSystemOutputDir/{DEM}_src.csv
-cp /usr/src/app/Catchment/{DEM}_HAND.tif $_tapisExecSystemOutputDir/{DEM}_HAND.tif
-cp /usr/src/app/Catchment/{DEM}_segment_catchments.tif $_tapisExecSystemOutputDir/{DEM}_segment_catchments.tif
+cp Catchment/{DEM}_src.csv $_tapisExecSystemOutputDir/{DEM}_src.csv
+cp Catchment/{DEM}_HAND.tif $_tapisExecSystemOutputDir/{DEM}_HAND.tif
+cp Catchment/{DEM}_segment_catchments.tif $_tapisExecSystemOutputDir/{DEM}_segment_catchments.tif

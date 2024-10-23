94a72717a3ab9c97a5d834e0cae1c03d5655def3
tobiashi26 <tobiashoughton2@gmail.com>
2024-10-23 14:41:24 -0500

    Remvoing / before /Catchment
    changing path from /Catchment to Catchment where Catchment refers to filepath containing input data

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ DEM=$1
 # Print the parameters for debugging
 echo "DEM: ${DEM}"
 
-python /code/main.py /Catchment/${DEM}.tif /Catchment/Catchment.shp /Catchment/Flowlines.shp /Catchment/Streamflow
+python /code/main.py Catchment/${DEM}.tif Catchment/Catchment.shp Catchment/Flowlines.shp Catchment/Streamflow
 
-cp -r /Catchment/Inundation $_tapisExecSystemOutputDir/Inundation
+cp -r Catchment/Inundation $_tapisExecSystemOutputDir/Inundation

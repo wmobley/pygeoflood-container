52667931d5cb5fcd7d0bb95c1bd540c13242e3c8
tobiashi26 <tobiashoughton2@gmail.com>
2024-10-23 21:10:49 -0500

    swithcing fim_list input in run.sh to take all the arguments from 3 onwards

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ fim_list=$3
 # Print the parameters for debugging
 echo "DEM: ${DEM}"
 echo "Points: ${Points}"
-echo "Points: ${fim_list}"
+echo "FIM List: ${fim_list}"
 
 python /code/main.py Catchment/${DEM}.tif Catchment/Catchment.shp Catchment/Flowlines.shp Catchment/Streamflow Catchment/Points/${Points}.shp "$fim_list"
 

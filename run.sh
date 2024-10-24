d8e37e20a2144c0fdc6cb2f5fe06effe3227b177
tobiashi26 <tobiashoughton2@gmail.com>
2024-10-23 20:37:57 -0500

    echoing inputs for debuggin

--- run.sh
+++ run.sh
@@ -2,10 +2,15 @@
 set -xe
 
 DEM=$1
+Points=$2
+fim_list=$3
 
 # Print the parameters for debugging
 echo "DEM: ${DEM}"
+echo "Points: ${Points}"
+echo "Points: ${fim_list}"
 
-python /code/main.py Catchment/${DEM}.tif Catchment/Catchment.shp Catchment/Flowlines.shp Catchment/Streamflow
+python /code/main.py Catchment/${DEM}.tif Catchment/Catchment.shp Catchment/Flowlines.shp Catchment/Streamflow Catchment/Points/${Points}.shp "$fim_list"
 
 cp -r Catchment/Inundation $_tapisExecSystemOutputDir/Inundation
+cp -r Catchment/Points $_tapisExecSystemOutputDir/Points

f3ff113b80b9b4975b7cc8978903de36ba99c284
tobiashi26 <tobiashoughton2@gmail.com>
2024-11-04 12:00:59 -0600

    Adding streamflow directory as implicit input
    Adding Streamflow directory as an implicit input in the .json file and adjusting run.sh to take additional argument

--- run.sh
+++ run.sh
@@ -2,15 +2,16 @@
 set -xe
 
 DEM=$1
-Points=$2
-fim_list=$3
+Streamflow=$2
+Points=$3
+fim_list=$4
 
 # Print the parameters for debugging
 echo "DEM: ${DEM}"
 echo "Points: ${Points}"
 echo "FIM List: ${fim_list}"
 
-python /code/main.py Catchment/${DEM}.tif Catchment/Catchment.shp Catchment/Flowlines.shp Catchment/Streamflow ${Points} "$fim_list"
+python /code/main.py Catchment/${DEM}.tif Catchment/Catchment.shp Catchment/Flowlines.shp Catchment/${Streamflow} ${Points} "$fim_list"
 
 cp -r Catchment/Inundation $_tapisExecSystemOutputDir/Inundation
 cp -r Catchment/Points $_tapisExecSystemOutputDir/Points

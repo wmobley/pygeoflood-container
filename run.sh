6607aff7ea7a3eac96dc949e1c6ed3c03f9597ff
Will Mobley <feulmob+github@gmail.com>
2025-06-30 14:23:40 -0500

    RUn.sh and Dcoker file

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 set -xe
 
 Streamflow=$1
-
-python main.py --flow-volume $Streamflow
+ls
+pwd
+python ./main.py --flow-volume $Streamflow
 
 cp -r Catchment/Inundation $_tapisExecSystemOutputDir/Inundation
 cp -r Catchment/Points $_tapisExecSystemOutputDir/Points

defe2ee4cb1f2d609b43f2aca9d52272e3b4014d
Will Mobley <feulmob+github@gmail.com>
2025-07-18 13:00:31 -0500

    Update Gitignore and run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ ls
 pwd
 python /code/main.py --flow-volume $Streamflow
 
-gdalwarp -t_srs "+proj=longlat +datum=WGS84 +no_defs" data/dem_fim.tif data/dem_fim.wgs84.tif
+gdalwarp -t_srs "EPSG:4326" data/dem_fim.tif data/dem_fim.wgs84.tif
 rio cogeo create data/dem_fim.wgs84.tif $_tapisExecSystemOutputDir/dem_fim.tif
 # cp -r data $_tapisExecSystemOutputDir
 
